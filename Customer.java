6fac219355a647d274f98eaf09b3ac54df6bcfdc
Gwonam <arvid.akerblom@gmail.com>
2017-05-11 18:05:16 +0200

    Added finished Customer.java
    megadase

--- Customer.java
+++ Customer.java
@@ -1,4 +1,31 @@
 
 public class Customer {
-
+	private String name;
+	private long idNr;
+	private static int customerNrCount = 1000;
+	private int customerNr;
+	
+	
+	public Customer(String name, long idNr) {
+		this.name = name;
+		this.idNr = idNr;
+		customerNrCount++;
+		customerNr = customerNrCount;
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	long getIdNr() {
+		return idNr;
+	}
+	
+	int getCustomerNr() {
+		return customerNr;
+	}
+	
+	public String toString() {
+		return name + ", id " + idNr + ", kundnr " + this.customerNr;  
+	}
 }
