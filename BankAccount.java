184190ddfdffa5998fd240c1e96c7bc598540746
DouglasAlgotsson <douglas.algotsson@gmail.com>
2017-05-11 19:34:25 +0200

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -3,7 +3,7 @@ public class BankAccount {
 
 	private double balance;
 	private Customer holder;
-	private int accountNrCount;
+	private static int accountNrCount;
 	private int accountNr;
 
 	public BankAccount(String holderName, long holderId) {
