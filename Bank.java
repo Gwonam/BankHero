8e1b7a47547a8b84450ffae5d09b76fea01c050e
Gwonam <arvid.akerblom@gmail.com>
2017-05-12 12:52:07 +0200

    Add files via upload
    Updated

--- Bank.java
+++ Bank.java
@@ -1,4 +1,54 @@
+import java.util.ArrayList;
 
 public class Bank {
+	private ArrayList<BankAccount> accounts;
+
+	public Bank() {
+
+	}
+
+	public int addAccount(String holderName, long idNr) {
+		BankAccount addThisAccount;
+		if (findHolder(idNr) == null) {
+			addThisAccount = new BankAccount(new Customer(holderName, idNr));
+		} else {
+			addThisAccount = new BankAccount(findHolder(idNr));
+		}
+		accounts.add(addThisAccount);
+		return addThisAccount.getAccountNumber();
+	}
+
+	public Customer findHolder(long idNr) {
+
+	}
+
+	public boolean removeAccount(int number) {
+		boolean existed;
+		BankAccount removeThisAccount = findByNumber(number);
+
+		if (removeThisAccount == null) {
+			existed = false;
+		} else {
+			accounts.remove(removeThisAccount);
+			existed = true;
+		}
+		return existed;
+	}
+
+	public ArrayList<BankAccount> getAllAccounts() {
+		return accounts;
+	}
+
+	public BankAccount findByNumber(int accountNumber) {
+
+	}
+
+	public ArrayList<BankAccount> findAccountsForHolder(long idNr) {
+
+	}
+
+	public ArrayList<Customer> findByPartofName(String namePart) {
+
+	}
 
 }
